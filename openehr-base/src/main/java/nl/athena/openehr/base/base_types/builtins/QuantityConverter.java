e94165dd7bbfa1a79f0af1d07d13d5a28ea6a372
jhoeflaken <jhoeflaken@live.nl>
2024-08-23 23:27:30 +0200

    Implemented builtins interfaces.

---
+++ openehr-base/src/main/java/nl/athena/openehr/base/base_types/builtins/QuantityConverter.java
@@ -0,0 +1,24 @@
+package nl.athena.openehr.base.base_types.builtins;
+
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Quantity converter.
+ */
+public interface QuantityConverter {
+
+    /**
+     * Converts the given value from the given units to the given units.
+     *
+     * @param theValue     The value to convert.
+     * @param theFromUnits The units to convert from.
+     * @param theToUnits   The units to convert to.
+     * @param theProperty  The property to convert.
+     * @return The converted value.
+     */
+    Float convertValue(@NotNull final Float theValue,
+                       @NotNull final String theFromUnits,
+                       @NotNull final String theToUnits,
+                       @NotNull final TerminologyCode theProperty);
+
+}
