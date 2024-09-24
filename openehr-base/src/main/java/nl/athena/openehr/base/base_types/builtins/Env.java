912e8ba554184de9732d552057b6a2d088850db4
jhoeflaken <jhoeflaken@live.nl>
2024-09-24 08:02:56 +0200

    Completed definitions package.

--- openehr-base/src/main/java/nl/athena/openehr/base/base_types/builtins/Env.java
+++ openehr-base/src/main/java/nl/athena/openehr/base/base_types/builtins/Env.java
@@ -5,11 +5,39 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.TimeZone;
 
+/**
+ * Class representing the real-world environment, providing basic information like current time, date, etc. See
+ * <a href="https://specifications.openehr.org/releases/BASE/development/base_types.html#_env_interface">
+ *     Env</a> interface.
+ */
 public interface Env {
 
+    /**
+     * Return today’s date in the current locale.
+     *
+     * @return Today’s date in the current locale.
+     */
     LocalDate currentDate();
+
+    /**
+     * Return the current time in the current locale.
+     *
+     * @return The current time in the current locale.
+     */
     LocalTime currentTime();
+
+    /**
+     * Return the current date and time in the current locale.
+     *
+     * @return The current date and time in the current locale.
+     */
     LocalDateTime currentDateTime();
+
+    /**
+     * Return the time zone of the current locale.
+     *
+     * @return The time zone of the current locale.
+     */
     TimeZone currentTimeZone();
 
 }
--- openehr-base/src/main/java/nl/athena/openehr/base/base_types/builtins/Locale.java
+++ openehr-base/src/main/java/nl/athena/openehr/base/base_types/builtins/Locale.java
@@ -2,6 +2,11 @@ package nl.athena.openehr.base.base_types.builtins;
 
 import nl.athena.openehr.base.foundation_types.terminology.TerminologyCode;
 
+/**
+ * Class representing the locale. See
+ * <a href="https://specifications.openehr.org/releases/BASE/development/base_types.html#_locale_interface">
+ *     Locale</a> interface.
+ */
 public interface Locale {
 
     TerminologyCode primaryLanguage();
