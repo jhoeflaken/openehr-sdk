b2d135b10c4febcbef846f9872fb495b2d47bac9
jhoeflaken <jhoeflaken@live.nl>
2024-09-26 21:02:35 +0200

    Implemented Temporal

--- openehr-base/src/main/java/nl/athena/openehr/base/foundation_types/time/Temporal.java
+++ openehr-base/src/main/java/nl/athena/openehr/base/foundation_types/time/Temporal.java
@@ -7,35 +7,65 @@ package nl.athena.openehr.base.foundation_types.time;
  */
 public abstract class Temporal implements Comparable<Temporal> {
 
-    private Double millis;
-
-    public Double getMillis() {
-        return millis;
+    /**
+     * Arithmetic value comparison. Returns True if current object is less than the other. This operator is effected
+     * and/or redefined in descendants to provide the appropriate ordering semantics for concrete types. In conjunction
+     * with =, enables the definition of the related functions greater_than() etc.
+     *
+     * @param theOther The other Temporal object to compare with.
+     * @return True if current object is less than the other.
+     */
+    public boolean lessThan(final Temporal theOther) {
+        return compareTo(theOther) < 0;
     }
 
+    /**
+     * True if current object greater than the other.
+     *
+     * @param theOther The other Temporal object to compare with.
+     * @return True if current object greater than the other.
+     */
+    public boolean greaterThan(final Temporal theOther) {
+        return compareTo(theOther) > 0;
+    }
+
+    /**
+     * True if current object less than or equal to the other.
+     *
+     * @param theOther The other Temporal object to compare with.
+     * @return True if current object less than or equal to the other.
+     */
+    public boolean lessOrEqual(final Temporal theOther) {
+        return compareTo(theOther) <= 0;
+    }
+
+    /**
+     * True if current object greater than or equal to the other.
+     *
+     * @param theOther The other Temporal object to compare with.
+     * @return True if current object greater than or equal to the other.
+     */
+    public boolean greaterOrEqual(final Temporal theOther) {
+        return compareTo(theOther) >= 0;
+    }
+
+    /**
+     * True if current object equal to the other.
+     *
+     * @param theOther The other Temporal object to compare with.
+     * @return True if current object equal to the other.
+     */
     @Override
-    public int compareTo(final Temporal theOther) {
-        return millis.compareTo(theOther.getMillis());
-    }
+    public boolean equals(final Object theOther) {
+        if (this == theOther) {
+            return true;
+        }
 
-    public boolean lessThan(final Iso8601Type theOther) {
-        return millis < theOther.getMillis();
-    }
+        if (theOther == null || getClass() != theOther.getClass()) {
+            return false;
+        }
 
-    public boolean greaterThan(final Iso8601Type theOther) {
-        return millis > theOther.getMillis();
-    }
-
-    public boolean lessOrEqual(final Iso8601Type theOther) {
-        return millis <= theOther.getMillis();
-    }
-
-    public boolean greaterOrEqual(final Iso8601Type theOther) {
-        return millis >= theOther.getMillis();
-    }
-
-    public boolean equals(final Iso8601Type theOther) {
-        return millis == theOther.getMillis();
+        return compareTo((Temporal) theOther) == 0;
     }
 
 }
