c5b9b575a93bdb06d371844d954dfccd09d0acb1
jhoeflaken <jhoeflaken@live.nl>
2024-10-04 23:59:57 +0200

    Started working on the services

---
+++ openehr-its-core/src/main/java/nl/athena/openehr/its/core/service/DefaultEhrService.java
@@ -0,0 +1,95 @@
+package nl.athena.openehr.its.core.service;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import nl.athena.openehr.base.base_types.identification.ObjectVersionId;
+import nl.athena.openehr.base.base_types.identification.Uuid;
+import nl.athena.openehr.its.core.dto.EhrStatusDto;
+import nl.athena.openehr.rm.common.change_control.OriginalVersion;
+import nl.athena.openehr.rm.common.change_control.VersionedObject;
+import nl.athena.openehr.rm.common.generic.RevisionHistory;
+import nl.athena.openehr.rm.data_types.quantity.date_time.DvDateTime;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+@Service
+public class DefaultEhrService implements EhrService {
+
+    @Override
+    public EhrResult create(@Nullable Uuid theEhrId, @Nullable EhrStatusDto theStatus) {
+        return null;
+    }
+
+    @Override
+    public EhrResult updateStatus(@Nonnull Uuid theEhrId, @Nonnull EhrStatusDto theStatus, @Nonnull ObjectVersionId theTargetObjectId, @Nonnull Uuid theContributionId, @Nonnull Uuid theAuditId) {
+        return null;
+    }
+
+    @Override
+    public EhrResult getStatus(@Nonnull Uuid theEhrId) {
+        return null;
+    }
+
+    @Override
+    public Optional<OriginalVersion<EhrStatusDto>> getEhrStatusAtVersion(@Nonnull Uuid theEhrId, @Nonnull Uuid theVersionedObjectId, int theVersion) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Uuid> findBySubjectId(@Nonnull String theSubjectId, @Nonnull String theSubjectNamespace) {
+        return Optional.empty();
+    }
+
+    @Override
+    public ObjectVersionId getLatestVersionUidOfStatus(@Nonnull Uuid theEhrId) {
+        return null;
+    }
+
+    @Override
+    public ObjectVersionId getEhrStatusVersionByTimestamp(@Nonnull Uuid theEhrId, @Nonnull OffsetDateTime theTimestamp) {
+        return null;
+    }
+
+    @Override
+    public DvDateTime getCreationTime(@Nonnull Uuid theEhrId) {
+        return null;
+    }
+
+    @Override
+    public boolean hasEhr(@Nonnull Uuid theEhrId) {
+        return false;
+    }
+
+    @Override
+    public VersionedObject<EhrStatusDto> getVersionedEhrStatus(@Nonnull Uuid theEhrId) {
+        return null;
+    }
+
+    @Override
+    public RevisionHistory getRevisionHistoryOfVersionedEhrStatus(@Nonnull Uuid theEhrId) {
+        return null;
+    }
+
+    @Override
+    public void delete(@Nonnull Uuid theEhrId) {
+
+    }
+
+    @Override
+    public String getSubjectExternalRef(@Nonnull Uuid theEhrId) {
+        return "";
+    }
+
+    @Override
+    public void checkEhrExists(@Nonnull Uuid theEhrId) {
+
+    }
+
+    @Override
+    public void checkEhrExistsAndIsModifiable(@Nonnull Uuid theEhrId) {
+
+    }
+
+}
